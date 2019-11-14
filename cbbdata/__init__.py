963e907dbe36be5fcfffab4ff262db34c9cbffcd
TeneoPython01 <TeneoPython01@gmail.com>
2019-11-13 21:52:53 -0600

    NEW MODULE FOR CBB DATA PULLS Added skeleton for cbb data pull class

---
+++ cbbdata/__init__.py
@@ -0,0 +1,16 @@
+# College Basketball Data Class Module
+#
+# Author(s):        TeneoPython01@gmail.com
+#
+# Purpose:          This script is used to establish the class
+#
+# Special Notes:    n/a
+
+#establish the class
+class cbbData(object):
+
+    #__init__() "MAGIC METHOD" sets the current season
+    #and then runs a couple methods to set up class
+    #properties using the config file and cfb APIs
+    def __init__(self):
+        print('hello')
