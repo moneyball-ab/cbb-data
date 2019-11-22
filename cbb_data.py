d2805c0b7d0919b84b6c7395bad985d77d3d4086
TeneoPython01 <TeneoPython01@gmail.com>
2019-11-21 21:53:47 -0600

    BASIC SCHEDULE INGESTION
    Set up basic ability to ingest schedule data.  This
    will be expanded over time, but this places the
    bare bones in place.

--- cbb_data.py
+++ cbb_data.py
@@ -1,53 +1,25 @@
 # College Basketball Script
 #
 # Author(s):        TeneoPython01@gmail.com
-# Developed:        10/11/2019
-# Last Updated:     10/11/2019
 #
 # Purpose:          This script is used to run the program
 #
-# Special Notes:    This is an early protoype
+# Special Notes:    n/a
 
 import pandas as pd #to handle data in pandas dataframes
 pd.set_option('display.max_rows', 1000) #allow printing lots of rows to screen
 pd.set_option('display.max_columns', 1000) #allow printsin lots of cols to screen
 pd.set_option('display.width', 1000) #don't wrap lots of columns
 
-import requests #to pull data
-import numpy as np #to do conditional pandas operations
+from pyquery import PyQuery as pq
+import pandas as pd
+import cbbdata as cbb
+import dbutil as dbutil
 
-#define team and last year of the season of interest
-#e.g., 2018-2019 season is last_year = 2019
-team = 'Wake Forest'
-last_year = 2019
+db = dbutil.Database()
+cbb_data = cbb.cbbData()
 
-#format team string to be compatible with URL request
-team = team.lower().replace(' ','-')
+#load the watchlist schedules to the database
+db.ingest(cbb_data.watchlist_schedule, 'cbb_schedule_tmp', 'replace')
 
-#request URL with schedule data
-r = requests.get(
-    'https://www.sports-reference.com/cbb/schools/' +
-    team +
-    '/' +
-    str(last_year) +
-    '-schedule.html'
-).text
-
-#load the HTML table data into pandas dataframe
-df = pd.DataFrame()
-df = pd.read_html(r)[1]
-
-print(
-    df[
-        [
-            'G',
-            'Date',
-            'Time',
-            'Type',
-            'Opponent',
-            'Unnamed: 7',
-            'Tm',
-            'Opp'        
-        ]
-    ]
-)
+#print(cbb_data.watchlist_schedule)
--- cbbdata/__init__.py
+++ cbbdata/__init__.py
@@ -6,6 +6,62 @@
 #
 # Special Notes:    n/a
 
+
+
+from pyquery import PyQuery as pq
+import pandas as pd
+
+
+
+
+def make_list(var):
+    if type(var) is not list:
+        var = [ var ]
+
+    return(var)
+
+def rename_col(df, old_field_name, new_field_name):
+    df = df.rename(
+        columns = {
+            old_field_name: new_field_name
+        }
+    )
+    
+    return df
+
+def drop_col(df, fields_to_drop):
+    fields_to_drop = make_list(fields_to_drop)
+    
+    df = df.drop(
+        labels=fields_to_drop,
+        axis=1
+    )
+    
+    return df
+
+def reorder_cols(df, new_order_list):
+    old_cols_list = df.columns.tolist()
+    
+    old_order_list = list(range(len(old_cols_list)))
+    new_order_list = make_list(new_order_list)
+    
+    num_old_cols = len(old_cols_list)
+    num_new_cols = len(new_order_list)
+    
+    new_cols_list = []
+
+    count = 0
+    for old_item in old_cols_list:
+        new_cols_list.append(old_cols_list[new_order_list[count]])
+        count = count + 1
+    
+    df = df[new_cols_list]
+    
+    return df
+
+
+
+
 #establish the class
 class cbbData(object):
 
@@ -13,4 +69,46 @@ class cbbData(object):
     #and then runs a couple methods to set up class
     #properties using the config file and cfb APIs
     def __init__(self):
-        print('hello')
+        self.team_list = []
+        self.__load_team_list()
+        
+        self.season_list = []
+        self.__load_season_list()
+        
+        self.watchlist_schedule = self.getSchedule(self.team_list, self.season_list)
+    
+    def __load_team_list(self):
+        self.team_list.append('wake-forest')
+        self.team_list.append('georgia')
+    
+    def __load_season_list(self):
+        self.season_list.append(2019)
+    
+    def getSchedule(self, team_list, season_list):
+        team_list = make_list(team_list)
+        season_list = make_list(season_list)
+        
+        df = pd.DataFrame()
+        
+        for team_item in team_list:
+            for season_item in season_list:
+                #load pq object from schedule page
+                url = 'https://www.sports-reference.com/cbb/schools/{}/{}-schedule.html'.format(team_item, str(season_item))
+                html_page = pq(url)
+                div = 'table#schedule'
+                
+                #grab the html for the schedule table
+                html_str = html_page(div).outerHtml()
+
+                #create a df from the html
+                df_temp = pd.read_html(html_str)[0]
+
+                #add fields
+                df_temp['season'] = season_item
+                df_temp['team'] = team_item
+                
+                df = df.append(df_temp)
+        
+        df = df.reset_index(drop=True)
+        
+        return df
